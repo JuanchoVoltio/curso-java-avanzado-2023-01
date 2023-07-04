272e7c93365e5c24db617fb709c983f0e47720ee
JuanchoVoltio <henryl.gomez@gmail.com>
2023-07-04 17:20:26 -0500

    Agregar el método para guardar un paciente.

--- centro-medico-v2/src/persistencia/IObjetoDeAcessoADatos.java
+++ centro-medico-v2/src/persistencia/IObjetoDeAcessoADatos.java
@@ -1,6 +1,7 @@
 package persistencia;
 
 import modelo.impl.Medico;
+import modelo.impl.Paciente;
 
 public interface IObjetoDeAcessoADatos { //Data Access Object
 
@@ -10,5 +11,8 @@ public interface IObjetoDeAcessoADatos { //Data Access Object
      * @return true si guardó el médico, false si el médico ya estaba registrado, en cuyo caso m no será guardado.
      */
     boolean guardarMedico(Medico m);
+
+    boolean guardarPaciente(Paciente p);
+
     //TODO: Completar los otros métodos.
 }
